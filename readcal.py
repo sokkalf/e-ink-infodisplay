da17464080213f6dc7add9cb2364efb3d3fa2f79
Christian Lønaas <christian.lonaas@discombobulator.org>
2018-02-12 22:31:48 +0100

    Convert events from UTC to localtime

--- readcal.py
+++ readcal.py
@@ -2,6 +2,8 @@
 
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
+from dateutil.parser import parse
+from dateutil import tz
 import vobject
 import caldav
 
@@ -17,7 +19,8 @@ class Readcal:
         events = []
         for r in results:
             event = vobject.readOne(r.data)
-            events = events + [(event.vevent.dtstart.valueRepr(),event.vevent.summary.valueRepr().decode('utf8'))]
+            timestamp = parse(str(event.vevent.dtstart.valueRepr())).astimezone(tz.tzlocal())
+            events = events + [(timestamp,event.vevent.summary.valueRepr().decode('utf8'))]
 
         events = sorted(events, key=lambda tup: tup[0])
         return events
