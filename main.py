3eaf5ed4ed02d0aa2842775883b371c511f252e9
Christian Lønaas <christian.lonaas@discombobulator.org>
2018-02-12 21:32:49 +0100

    Compare displayed image with previous, and only display if changed

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import epd7in5
 from PIL import Image
 from PIL import ImageDraw
 from PIL import ImageFont
+
 from yr import Yr
 from readcal import Readcal
 import yaml
@@ -13,8 +14,10 @@ import time
 EPD_WIDTH = 640
 EPD_HEIGHT = 384
 
+prev_image_hash = None
 
 def display_info(display, yr, cal):
+    global prev_image_hash
     weather_symbol = Image.open(yr.get_weather_symbol())
     image = Image.open('img/main.bmp')
     draw = ImageDraw.Draw(image)
@@ -30,7 +33,12 @@ def display_info(display, yr, cal):
         offset_calendar_text_pos = offset_calendar_text_pos + 25
 
     image = image.rotate(90, False, True)
-    display.display_frame(display.get_frame_buffer(image))
+    image_hash = hash(image.tobytes())
+    print image_hash
+    if prev_image_hash != image_hash:
+        display.display_frame(display.get_frame_buffer(image))
+
+    prev_image_hash = image_hash
 
 
 def main():
