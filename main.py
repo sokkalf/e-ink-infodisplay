fa7d63cc8cb746dd5a56faf821c673eb8356ff02
Christian Lønaas <christian.lonaas@discombobulator.org>
2018-02-12 21:03:34 +0100

    Run in loop

--- main.py
+++ main.py
@@ -7,16 +7,14 @@ from PIL import ImageFont
 from yr import Yr
 from readcal import Readcal
 import yaml
+import time
+
 
 EPD_WIDTH = 640
 EPD_HEIGHT = 384
 
-def main():
-    epd = epd7in5.EPD()
-    epd.init()
-    config = yaml.load(open('config.yml'))
-    yr = Yr(config['weather']['url'])
-    cal = Readcal(config['calendar']['url'], config['calendar']['username'], config['calendar']['password'])
+
+def display_info(display, yr, cal):
     weather_symbol = Image.open(yr.get_weather_symbol())
     image = Image.open('img/main.bmp')
     draw = ImageDraw.Draw(image)
@@ -32,7 +30,19 @@ def main():
         offset_calendar_text_pos = offset_calendar_text_pos + 25
 
     image = image.rotate(90, False, True)
-    epd.display_frame(epd.get_frame_buffer(image))
+    display.display_frame(display.get_frame_buffer(image))
+
+
+def main():
+    epd = epd7in5.EPD()
+    epd.init()
+    config = yaml.load(open('config.yml'))
+    yr = Yr(config['weather']['url'])
+    cal = Readcal(config['calendar']['url'], config['calendar']['username'], config['calendar']['password'])
+    while(True):
+        display_info(epd, yr, cal)
+        time.sleep(60)
+        yr.refresh()
 
     image = Image.open('img/skrik.bmp')
     epd.display_frame(epd.get_frame_buffer(image))
